8327a68cef043b09249893858a88a589e5ee758d
Sadman-123 <19201103123@cse.bubt.edu.bd>
2024-10-10 04:46:10 +0600

    Change text "Click me"

--- lib/app.dart
+++ lib/app.dart
@@ -1,47 +1,63 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:untitled/provider/count_provider.dart';
+
 class App extends StatelessWidget {
   const App({super.key});
   @override
   Widget build(BuildContext context) {
-    var mdw=MediaQuery.sizeOf(context).width;
-    var mdh=MediaQuery.sizeOf(context).height;
+    var mdw = MediaQuery.sizeOf(context).width;
+    var mdh = MediaQuery.sizeOf(context).height;
     return Scaffold(
-      body: Center(
-        child: Consumer<CountProvider>(builder: (context, model, child) {
+      body: Center(child: Consumer<CountProvider>(
+        builder: (context, model, child) {
           return Column(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              InkWell(child: Text("${model.count}",style: TextStyle(fontSize: mdw*0.259),),onTap: (){model.decrem();},),
-              _mybutton(mdw,mdh,onPressed: (){model.increm();})
+              InkWell(
+                child: Text(
+                  "${model.count}",
+                  style: TextStyle(fontSize: mdw * 0.259),
+                ),
+                onTap: () {
+                  model.decrem();
+                },
+              ),
+              _mybutton(mdw, mdh, onPressed: () {
+                model.increm();
+              })
             ],
           );
-        },)
-      ),
+        },
+      )),
     );
   }
 }
-Widget _mybutton(double mdw,double mdh, {required VoidCallback onPressed})
-{
+
+Widget _mybutton(double mdw, double mdh, {required VoidCallback onPressed}) {
   return InkWell(
     onTap: onPressed,
     child: Container(
       child: Center(
-        child: InkWell(child: Text("Press Me",style: TextStyle(fontSize: mdw*0.05,color: Colors.white,fontWeight: FontWeight.w500),),),
+        child: InkWell(
+          child: Text(
+            "Click Me",
+            style: TextStyle(
+                fontSize: mdw * 0.05,
+                color: Colors.white,
+                fontWeight: FontWeight.w500),
+          ),
+        ),
       ),
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16),
-        gradient: LinearGradient(
-          colors: [
+          borderRadius: BorderRadius.circular(16),
+          gradient: LinearGradient(colors: [
             Colors.blue.shade600,
             Colors.blueAccent.shade200,
             Colors.blueAccent.shade200
-          ]
-        )
-      ),
-      height: mdh*0.075,
-      width: mdw*0.55,
+          ])),
+      height: mdh * 0.075,
+      width: mdw * 0.55,
     ),
   );
 }
